fbe10d7a85077bd61bc39b7950414ae6aeb9557f
Anastasia <akroyl419@gmail.com>
2026-01-12 00:43:09 +0200

    fix build

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -2,13 +2,16 @@ import CardList from '@/components/cardList/cardList'
 import css from './catalog.module.css'
 import { fetchCampers } from '@/lib/api/api'
 import Filter from '@/components/filters/filters'
+import { Suspense } from 'react'
 
 export default async function Catalog() {
     const campers = await fetchCampers()
 
     return (
         <section className={css.section}>
-            <Filter/>
+            <Suspense fallback={<div>Loading filters...</div>}>
+                <Filter/>
+            </Suspense>
             <CardList campers={campers.items} />
         </section>
     )
